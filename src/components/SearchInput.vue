57f694b76994ccf9460131d69a9bc8f7b928e768
haden96 <charkiewiczdaniel@gmail.com>
2020-06-29 14:44:17 +0200

    finished app

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="searchWrapper">
-    <input id="search" name="search" v-model="searchValue" @input="handleInput" />
+    <input id="search" name="search" :value="value" @input="handleChange" :class="{ dark }"/>
   </div>
 </template>
 
 <script>
 export default {
   name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -23,5 +38,13 @@ export default {
     background: none;
     border-bottom: 1px solid black;
   }
+}
+.dark{
+  color: #1e3d4a;
+  border-bottom-color:  #1e3d4a;
+}
+
+.dark:focus{
+  box-shadow: 0 10px 20px -8px rgba(#1e3d4a, .2);
 }
 </style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="wrapper">
-    <Claim />
-    <SearchInput />
+  <div :class="[{flexStart: step === 1}, 'wrapper']">
+    <transition name="slide">
+      <img src="../assets/logo.png" class="logo" v-if="step === 1">
+    </transition>
+    <transition name='fade'>
+      <HeroImage v-if='step === 0' />
+    </transition>
+    <Claim v-if='step === 0' />
+    <SearchInput v-model='searchValue' @input='handleInput' :dark='step===1' />
+    <div class="results" v-if="results && !loading && step === 1">
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id"
+      @click.native="handleModalOpen(item)"/>
+    </div>
+    <div class="loader" v-if="step === 1 && loading"/>
+    <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false"/>
   </div>
 </template>
 
@@ -10,28 +22,46 @@ import axios from 'axios';
 import debounce from 'lodash.debounce';
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
+import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
+import Modal from '@/components/Modal.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
 export default {
   name: 'Search',
   components: {
+    HeroImage,
     Claim,
     SearchInput,
+    Item,
+    Modal,
   },
   data() {
     return {
+      modalOpen: false,
+      modalItem: null,
+      loading: false,
+      step: 0,
       searchValue: '',
       results: [],
     };
   },
   methods: {
+    handleModalOpen(item) {
+      this.modalOpen = true;
+      this.modalItem = item;
+    },
     // eslint-disable-next-line
     handleInput: debounce(function() {
+      this.loading = true;
+      console.log(this.searchValue);
       axios
         .get(`${API}?q=${this.searchValue}&media_type=image`)
         .then((response) => {
           this.results = response.data.collection.items;
+          this.loading = false;
+          this.step = 1;
         })
         .catch((error) => {
           console.log(error);
@@ -41,19 +71,71 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter, .fade-leave-to
+/* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.loader {
+  margin-top: 100px;
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.loader:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid black;
+  border-color: black transparent black transparent;
+  animation: loading 1.2s linear infinite;
+}
 .wrapper {
   margin: 0;
   flex-direction: column;
   align-items: center;
+  position: relative;
   padding: 30px;
   width: 100%;
+  min-height: 100vh;
   height: 100vh;
   padding: 30px;
   justify-content: center;
-  background-image: url('../assets/heroimage.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 80% 0%;
+  display: flex;
+
+  &.flexStart {
+    justify-content: flex-start;
+  }
+
+  .logo{
+    position: absolute;
+    top: 30px;
+  }
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 0.3s ease;
+  }
+  .slide-enter, .slide-leave-to
+  /* .fade-leave-active below version 2.1.8 */ {
+    margin-top: -50px;
+
+  }
+}
+.results {
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+
+  @media (min-width: 768px){
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 }
 </style>
